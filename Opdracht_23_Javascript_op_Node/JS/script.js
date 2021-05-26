3437f30e49e12cfe505859298d7abc70f477cbc7
DFW2025 <81567386+DFW2025@users.noreply.github.com>
2021-05-26 15:38:47 +0200

    Add files via upload

---
+++ Opdracht_23_Javascript_op_Node/JS/script.js
@@ -0,0 +1,18 @@
+// Javascript
+
+
+
+console.log("Hallo Winc Academy Studenten")
+
+// Dit is een grote som
+
+/*  Dit zijn 2 rekensommen
+
+let multiplication = 1230941 * 1823794;
+console.log("multiplication:", multiplication);
+
+let division = 637263 / 54;
+console.log("division:", division);
+
+*/
+
